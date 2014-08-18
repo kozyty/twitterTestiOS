d3061c4b044c71bb737523f686b10b64165d4967
kozyty <kozyty@gmail.com>
2014-08-18 17:08:14 +0900

    Initial Commit

---
+++ twitterTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitterTest
+//
+//  Created by kozyty on 2014/08/18.
+//  Copyright (c) 2014年 nanapi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitterTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  twitterTest
+//
+//  Created by kozyty on 2014/08/18.
+//  Copyright (c) 2014年 nanapi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
