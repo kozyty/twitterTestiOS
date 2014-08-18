d9d0fc108411d86f6b000a83c9c7344b4b4395ea
kozyty <kozyty@gmail.com>
2014-08-18 17:47:16 +0900

    Init twitterTestApp

--- twitterTest/ViewController.m
+++ twitterTest/ViewController.m
@@ -7,10 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "STTwitterAPI.h"
 
 @interface ViewController ()
 
+@property (nonatomic, strong) STTwitterAPI *twitter;
+
+@property (strong, nonatomic) NSString *accessToken;
+@property (strong, nonatomic) NSString *accessTokenSecret;
+
 @end
+
+#define kConsumerName @"登録したApp名"
+#define kConsumerKey @"Developer登録したKey"
+#define kConsumerKeySecret @"Developer登録したSecretKey"
 
 @implementation ViewController
 
@@ -18,6 +28,38 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.twitter = [STTwitterAPI twitterAPIWithOAuthConsumerName:kConsumerName
+                                                              consumerKey:kConsumerKey
+                                                           consumerSecret:kConsumerKeySecret];
+    [self.twitter postReverseOAuthTokenRequest:^(NSString *authenticationHeader) {
+        self.twitter = [STTwitterAPI twitterAPIOSWithFirstAccount];
+        [self.twitter verifyCredentialsWithSuccessBlock:^(NSString *username) {
+            [self.twitter postReverseAuthAccessTokenWithAuthenticationHeader:authenticationHeader
+                                                                successBlock:^(NSString *oAuthToken,
+                                                                               NSString *oAuthTokenSecret,
+                                                                               NSString *userID,
+                                                                               NSString *screenName) {
+                                                                    self.accessToken = oAuthToken;
+                                                                    self.accessTokenSecret = oAuthTokenSecret;
+                                                                    NSLog(@"Token %@ secret %@",oAuthToken,oAuthTokenSecret);
+                                                                    
+                                                                } errorBlock:^(NSError *error) {
+                                                                    NSLog(@"error %@",[error description]);
+                                                                    
+                                                                }];
+        } errorBlock:^(NSError *error) {
+            NSLog(@"error %@",[error description]);
+        }];
+    } errorBlock:^(NSError *error) {
+        NSLog(@"error %@",[error description]);
+    }];
+    
+    [self.twitter getSearchTweetsWithQuery:@"nanapiのアンサー"
+                              successBlock:^(NSDictionary *searchMetadata, NSArray *statuses) {
+                                  NSLog(@"%@ %@", searchMetadata, statuses);
+                              } errorBlock:^(NSError *error) {
+                                  NSLog(@"%@", error);
+                              }];
 }
 
 - (void)didReceiveMemoryWarning
